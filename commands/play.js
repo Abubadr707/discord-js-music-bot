e755a74d7b0e36953a4395f7af49c89d057dfc45
Abubadr707 <abubadr707@gmail.com>
2024-12-15 10:22:05 +0300

    Update play.js
    g

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,8 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
+Mconst { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 const { QueryType } = require("discord-player")
+
+/play search <sons>
 
 module.exports = {
 	data: new SlashCommandBuilder()
